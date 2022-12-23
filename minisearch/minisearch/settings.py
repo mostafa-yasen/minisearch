d312de6b83ef0a2e9ce82218fa6214866f3cf90f
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2022-12-23 15:39:31 +0200

    Added settings for deployment static files

--- minisearch/minisearch/settings.py
+++ minisearch/minisearch/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'minisearch/static')]
-
+STATIC_ROOT = os.path.join(BASE_DIR, "minisearch/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
